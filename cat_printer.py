5f79dbdfc14d82e14f67bdb80ffeb068578a2240
Thrashy190 <diego456.dlm77@gmail.com>
2022-11-11 10:40:24 -0600

    changes to cat_printer func

--- cat_printer.py
+++ cat_printer.py
@@ -1,18 +1,30 @@
-def print_pet(times : int, cat: bool):
+def print_pet(times : int, cat: bool) -> str:
 
-    cat = '''
-        🐈            🐈
-        🐈               🐈
-        🐈             🐈🐈🐈  🐈
-        🐈             🐈🐈🐈🐈
-        🐈🐈🐈🐈🐈🐈🐈🐈🐈
-        🐈🐈🐈🐈🐈🐈🐈🐈🐈
-        🐈🐈🐈        🐈🐈
-        🐈🐈       🐈🐈
-            🐈🐈     🐈
-            🐈        🐈
-    '''
+    perro = "Ahora soy un perro guf guf"
 
-    print(cat)
+    return perro
 
-print_pet(2, True)
+print(print_pet(2, True))
+
+# Python 3.x code to demonstrate star pattern
+
+# Function to demonstrate printing pattern
+def pypart(n):
+	
+	# outer loop to handle number of rows
+	# n in this case
+	for i in range(0, n):
+	
+		# inner loop to handle number of columns
+		# values changing acc. to outer loop
+		for j in range(0, i+1):
+		
+			# printing stars
+			print("* ",end="")
+	
+		# ending line after each row
+		print("\r")
+
+# Driver Code
+n = 5
+pypart(n)
